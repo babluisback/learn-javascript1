ff53a896248baf22554b8a15a2b3434085106daa
Shahanawaj <bablualam2007@gmail.com>
2025-02-06 07:36:27 +0530

    even add check

---
+++ evenOdd.js
@@ -0,0 +1,20 @@
+function evenOdd(num) {
+  // console.log("this number is even");
+  if (num % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+evenOdd(2);
+///////////////////////////////////
+function isDevisibleByThree(num) {
+  // console.log("this number is even");
+  if (num % 3 == 0) {
+    console.log("true");
+  } else {
+    console.log("not");
+  }
+}
+isDevisibleByThree(7);
